74c571d0128eb381bdac7500977b9b8b199fe318
Brandon Chang <Brandon Chang>
2017-03-19 13:19:19 -0700

    Initial files, Added Firebase configuration and initialized it.

---
+++ assets/javascript/app.js
@@ -0,0 +1,14 @@
+ // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyD2BOmlinJl-NwyeIknq4hwGjhB2U9thtk",
+    authDomain: "escape-pods.firebaseapp.com",
+    databaseURL: "https://escape-pods.firebaseio.com",
+    storageBucket: "escape-pods.appspot.com",
+    messagingSenderId: "763288173467"
+  };
+  
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+  
