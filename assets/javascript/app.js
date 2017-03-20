562e8aaeb91569d6921c4a1567ec39a77546e3b3
Brandon Chang <Brandon Chang>
2017-03-19 21:29:36 -0700

    Pop-up attempt with fixed syntax

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -92,49 +92,50 @@ provider.addScope('https://www.googleapis.com/auth/plus.login');
 //   'login_hint': 'user@example.com'
 // });
 	function signIn() {
-		firebase.auth().getRedirectResult().then(function(result) {
-		  if (result.credential) {
-		    // This gives you a Google Access Token. You can use it to access the Google API.
-		    var token = result.credential.accessToken;
-		    // ...
-		  }
-		  // The signed-in user info.
-		  var user = result.user;
-		}).catch(function(error) {
-		  // Handle Errors here.
-		  var errorCode = error.code;
-		  var errorMessage = error.message;
-		  // The email of the user's account used.
-		  var email = error.email;
-		  // The firebase.auth.AuthCredential type that was used.
-		  var credential = error.credential;
-		  // ...
-		});
+		firebase.auth().signInWithPopup(provider).then(function(result) {
+  // This gives you a Google Access Token. You can use it to access the Google API.
+  var token = result.credential.accessToken;
+  // The signed-in user info.
+  var user = result.user;
+  // ...
+}).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // The email of the user's account used.
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+  // ...
+});
+firebase.auth().signOut().then(function() {
+  // Sign-out successful.
+}).catch(function(error) {
+  // An error happened.
+});
+		// firebase.auth().getRedirectResult().then(function(result) {
+		//   if (result.credential) {
+		//     // This gives you a Google Access Token. You can use it to access the Google API.
+		//     var token = result.credential.accessToken;
+		//     // ...
+		//   }
+		//   // The signed-in user info.
+		//   var user = result.user;
+		// }).catch(function(error) {
+		//   // Handle Errors here.
+		//   var errorCode = error.code;
+		//   var errorMessage = error.message;
+		//   // The email of the user's account used.
+		//   var email = error.email;
+		//   // The firebase.auth.AuthCredential type that was used.
+		//   var credential = error.credential;
+		//   // ...
+		// });
 	}
 
 
 
-// firebase.auth().signInWithPopup(provider).then(function(result) {
-//   // This gives you a Google Access Token. You can use it to access the Google API.
-//   var token = result.credential.accessToken;
-//   // The signed-in user info.
-//   var user = result.user;
-//   // ...
-// }).catch(function(error) {
-//   // Handle Errors here.
-//   var errorCode = error.code;
-//   var errorMessage = error.message;
-//   // The email of the user's account used.
-//   var email = error.email;
-//   // The firebase.auth.AuthCredential type that was used.
-//   var credential = error.credential;
-//   // ...
-// });
-// firebase.auth().signOut().then(function() {
-//   // Sign-out successful.
-// }).catch(function(error) {
-//   // An error happened.
-// });
+
 
 		// 	var podMomentStart = moment.unix(podStart).format("HH:mm")
 		// console.log(podMomentStart)
